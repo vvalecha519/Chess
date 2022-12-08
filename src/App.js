f3fcbb0d45156303e129defc8b0ac7b70f1b78bb
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2022-12-08 00:32:33 -0500

    finished creating board

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import {Chess} from "chess.js";
 import { Chessboard } from "react-chessboard";
 import ReactDOM from 'react-dom';
 
+import { firstRow, blackPieces, whitePieces } from "./pieces";
 import Draggable from "react-draggable";
-import chessPiece from "./assets/Chess_bdt60.png";
 import chessboard from "./assets/chessboard.png";
 import "./App.css";
 
 export default function App() {
-  const bound = {left: 25, top: 25, right:525, bottom: 525}
+  const bound = {left: 0, top: 0, right:525, bottom: 525}
   const logger = (event) => console.log(event);
-  return <div><img src={chessboard} alt="chessboard" className="chessboard" />
-    <Draggable
-  defaultPosition={{x: 20, y: 20}}
-  bounds={bound}
-  scale={1}
-  allowAnyClick={true}
-  onStop={logger}
-  id="draggable"
-  >
-<img src={chessPiece} alt="coffee" />
-</Draggable>
+  const getPieceImage = (i, type) => {
+    if ( i < 16) {
+      return blackPieces[type]
+    } else {
+      return whitePieces[type]
+    }
+  }
+  return <div className="container"><img src={chessboard} alt="chessboard" className="chessboard" />
+{
+    firstRow.map(function (v, i) {
+      return <Draggable
+      defaultPosition={{x: 23+ 70* (i%8), y: 23+ 70*Math.floor(i/8) }}
+      bounds={bound}
+      scale={1}
+      allowAnyClick={true}
+      onStop={logger}
+      id="draggable"
+      >
+    <img src={getPieceImage(i, v)} alt="coffee" className="piece" />
+    </Draggable>
+    })
+  }
+  {
+    firstRow.map(function (v, i) {
+      return <Draggable
+      defaultPosition={{x: 23+ 70* (i%8), y: 525 - 70*Math.floor(i/8) }}
+      bounds={bound}
+      scale={1}
+      allowAnyClick={true}
+      onStop={logger}
+      id="draggable"
+      >
+    <img src={getPieceImage(i+16, v)} alt="coffee" className="piece" />
+    </Draggable>
+    })
+  }
 </div>
 }
---
+++ src/pieces.js
@@ -0,0 +1,37 @@
+import rook from "../src/assets/Chess_rdt60.png";
+import knight from "../src/assets/Chess_ndt60.png";
+import bishop from "../src/assets/Chess_bdt60.png";
+import king from "../src/assets/Chess_kdt60.png";
+import queen from "../src/assets/Chess_qdt60.png";
+import pawn from "../src/assets/Chess_pdt60.png";
+
+import rook2 from "../src/assets/Chess_rlt60.png";
+import knight2 from "../src/assets/Chess_nlt60.png";
+import bishop2 from "../src/assets/Chess_blt60.png";
+import king2 from "../src/assets/Chess_klt60.png";
+import queen2 from "../src/assets/Chess_qlt60.png";
+import pawn2 from "../src/assets/Chess_plt60.png";
+
+
+const firstRow = ["r", "n", "b", "k", "q", "b","n", "r",
+"p", "p", "p", "p", "p", "p","p", "p"];
+
+const blackPieces = {
+"r": rook,
+"n": knight,
+"b": bishop,
+"k": king,
+"q": queen,
+"p": pawn
+}
+
+const whitePieces = {
+  "r": rook2,
+  "n": knight2,
+  "b": bishop2,
+  "k": king2,
+  "q": queen2,
+  "p": pawn2
+  }
+
+export {firstRow, blackPieces, whitePieces }
