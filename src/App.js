d5bdbaa70532279ec2ec629e5dd059355d35db5f
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2022-12-07 22:25:56 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {Chess} from "chess.js";
+import { Chessboard } from "react-chessboard";
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Draggable from "react-draggable";
+import chessPiece from "./assets/Chess_bdt60.png";
+import chessboard from "./assets/chessboard.png";
+import "./App.css";
+
+export default function App() {
+  const bound = {left: 25, top: 25, right:525, bottom: 525}
+  const logger = (event) => console.log(event);
+  return <div><img src={chessboard} alt="chessboard" className="chessboard" />
+    <Draggable
+  defaultPosition={{x: 20, y: 20}}
+  bounds={bound}
+  scale={1}
+  allowAnyClick={true}
+  onStop={logger}
+  id="draggable"
+  >
+<img src={chessPiece} alt="coffee" />
+</Draggable>
+</div>
 }
-
-export default App;
