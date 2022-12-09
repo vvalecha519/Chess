a0675e79aa764f592d0ef939b6e01ba511f427ac
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2022-12-08 23:51:51 -0500

    nothing really

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import chessboard from "./assets/chessboard.png";
 import "./App.css";
 
 export default function App() {
+  const startingPosition = {x: 23, y: 23}
+  const boxSize = 70
   const bound = {left: 0, top: 0, right:525, bottom: 525}
   const logger = (event) => console.log(event);
+  const handleStop = (e, data) => {
+    console.log(data)
+  }
+  
   const getPieceImage = (i, type) => {
     if ( i < 16) {
       return blackPieces[type]
@@ -22,11 +28,11 @@ export default function App() {
 {
     firstRow.map(function (v, i) {
       return <Draggable
-      defaultPosition={{x: 23+ 70* (i%8), y: 23+ 70*Math.floor(i/8) }}
+      defaultPosition={{x: startingPosition.x+ boxSize* (i%8), y: startingPosition.x+ boxSize*Math.floor(i/8) }}
       bounds={bound}
       scale={1}
       allowAnyClick={true}
-      onStop={logger}
+      onStop={handleStop}
       id="draggable"
       >
     <img src={getPieceImage(i, v)} alt="coffee" className="piece" />
@@ -36,12 +42,15 @@ export default function App() {
   {
     firstRow.map(function (v, i) {
       return <Draggable
-      defaultPosition={{x: 23+ 70* (i%8), y: 525 - 70*Math.floor(i/8) }}
+      defaultPosition={{x: startingPosition.x+ boxSize* (i%8), y: 515 - boxSize*Math.floor(i/8) }}
       bounds={bound}
       scale={1}
       allowAnyClick={true}
-      onStop={logger}
+      //onStart={logger}
+      onDrag={logger}
+      //onStop={logger}
       id="draggable"
+      grid={[boxSize, boxSize]}
       >
     <img src={getPieceImage(i+16, v)} alt="coffee" className="piece" />
     </Draggable>
