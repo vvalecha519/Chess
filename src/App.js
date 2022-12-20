ceab73b4a75d43cb78530a1c10bb78a27771a0ed
Vaibhav Valecha <vvalecha@uwaterloo.ca>
2022-12-19 23:41:23 -0500

    formatted files

--- src/App.js
+++ src/App.js
@@ -1,89 +1,104 @@
 import { useState } from "react";
-import {Chess} from "chess.js";
+import { Chess } from "chess.js";
 import { firstRow, blackPieces, whitePieces } from "./pieces";
 import chessboard from "./assets/chessboard.png";
 import "./App.css";
 import Piece from "./Piece";
 
 export default function App() {
-  const startingPosition = {x: 23, y: 23}
-  const boxSize = 70
-  const [chess, setChess] = useState(new Chess())
-  const [modifiedPiece, setModifiedPiece] = useState(null)
-  const [promotionPiece, setPromotionPiece] = useState(null)
-  const [currentPromotionPiece, setCurrentPromotionPiece] = useState(null)
+  const startingPosition = { x: 23, y: 23 };
+  const boxSize = 70;
+  const [chess, setChess] = useState(new Chess());
+  const [modifiedPiece, setModifiedPiece] = useState(null);
+  const [promotionPiece, setPromotionPiece] = useState(null);
+  const [currentPromotionPiece, setCurrentPromotionPiece] = useState(null);
   const getPieceImage = (i, type) => {
-    if ( i < 16) {
-      return blackPieces[type]
+    if (i < 16) {
+      return blackPieces[type];
     } else {
-      return whitePieces[type]
+      return whitePieces[type];
     }
-  }
+  };
 
   const callBack = (color, location) => {
-    if ( chess.isCheckmate() ) {
-      alert("Checkmate")
-    } else if ( chess.isCheck() ) {
-      alert("Check")
+    if (chess.isCheckmate()) {
+      alert("Checkmate");
+    } else if (chess.isCheck()) {
+      alert("Check");
     }
-    setModifiedPiece({color:color, location:location})
-  }
+    setModifiedPiece({ color: color, location: location });
+  };
 
   const promotionCallBack = (color, location) => {
-    console.log("promotion")
-    let input = document.getElementById("input")
-    input.disabled = false
-    console.log(location)
-   setCurrentPromotionPiece({color:color, location:location})
-  }
-
-  const resetCurrentPromotionPiece = () => {
-    
-  }
+    console.log("promotion");
+    let input = document.getElementById("input");
+    input.disabled = false;
+    console.log(location);
+    setCurrentPromotionPiece({ color: color, location: location });
+  };
 
   const handleChange = (event) => {
-    let input = document.getElementById("input")
-    setPromotionPiece({color:currentPromotionPiece.color, location:currentPromotionPiece.location, type:event.target.value})
-    input.value = ""
-    input.disabled = true
-  }
+    let input = document.getElementById("input");
+    setPromotionPiece({
+      color: currentPromotionPiece.color,
+      location: currentPromotionPiece.location,
+      type: event.target.value
+    });
+    input.value = "";
+    input.disabled = true;
+  };
 
-  return <div className="container"><img src={chessboard} alt="chessboard" className="chessboard" />
-{
-    firstRow.map(function (v, i) {
-      return <Piece
-      i={i}
-      source={getPieceImage(i, v)}
-      defaultPosition={{x: startingPosition.x+ boxSize* (i%8), y: startingPosition.x+ boxSize*Math.floor(i/8)  }}
-      chess={chess}  
-      mP={modifiedPiece}
-      callBack={callBack}
-      color={"black"}
-      promotionCallBack={promotionCallBack}
-      pP={promotionPiece}
-      pieceType={v}
-      >
-    </Piece>
-    })
-  }
-  {
-    firstRow.map(function (v, i) {
-      return <Piece
-      i={i}
-      source={getPieceImage(i+16, v)}
-      defaultPosition={{x: startingPosition.x+ boxSize* (i%8), y: 515 - boxSize*Math.floor(i/8) }}
-      chess={chess}  
-      mP={modifiedPiece}
-      callBack={callBack}
-      color={"white"}
-      promotionCallBack={promotionCallBack}
-      pP={promotionPiece}
-      pieceType={v}
-      >
-    </Piece>
-    })
-  }
-  <input type="text" id="input" placeholder="Enter letter of piece" disabled={true} onChange={handleChange} />
-  <button onClick={() => console.log(chess.ascii())}>View Chess Model</button>
-</div>
+  return (
+    <div className="container">
+      <img src={chessboard} alt="chessboard" className="chessboard" />
+      {firstRow.map(function (v, i) {
+        return (
+          <Piece
+            i={i}
+            source={getPieceImage(i, v)}
+            defaultPosition={{
+              x: startingPosition.x + boxSize * (i % 8),
+              y: startingPosition.x + boxSize * Math.floor(i / 8)
+            }}
+            chess={chess}
+            mP={modifiedPiece}
+            callBack={callBack}
+            color={"black"}
+            promotionCallBack={promotionCallBack}
+            pP={promotionPiece}
+            pieceType={v}
+          ></Piece>
+        );
+      })}
+      {firstRow.map(function (v, i) {
+        return (
+          <Piece
+            i={i}
+            source={getPieceImage(i + 16, v)}
+            defaultPosition={{
+              x: startingPosition.x + boxSize * (i % 8),
+              y: 515 - boxSize * Math.floor(i / 8)
+            }}
+            chess={chess}
+            mP={modifiedPiece}
+            callBack={callBack}
+            color={"white"}
+            promotionCallBack={promotionCallBack}
+            pP={promotionPiece}
+            pieceType={v}
+          ></Piece>
+        );
+      })}
+      <input
+        type="text"
+        id="input"
+        placeholder="Enter letter of piece"
+        disabled={true}
+        onChange={handleChange}
+      />
+      <button onClick={() => console.log(chess.ascii())}>
+        View Chess Model
+      </button>
+    </div>
+  );
 }
--- src/Piece.js
+++ src/Piece.js
@@ -1,86 +1,97 @@
 import { useState, useEffect } from "react";
 import Draggable from "react-draggable";
-import { firstRow, blackPieces, whitePieces } from "./pieces";
+import { blackPieces, whitePieces } from "./pieces";
 
-export default function Piece({ i, source, defaultPosition, chess, mP, callBack, color, promotionCallBack, pP, pieceType }) {
-  const [x, setX]= useState(defaultPosition.x)
-  const [y, setY]= useState(defaultPosition.y)
-  const [chessLogic, setChessLogic]= useState(chess)
-  const [modifiedPiece, setModifiedPiece] = useState(mP)
-  const [promotionPiece, setPromotionPiece] = useState(pP)
-  const [pieType, setPieceType] = useState(pieceType)
-  const [oldPie, setOldPiece] = useState("")
-  const startingPosition = {x: 23, y: 23}
-  const boxSize = 70
-  const bound = {left: 0, top: 0, right:525, bottom: 525} 
+export default function Piece({i, source, defaultPosition, chess, mP, callBack, color, promotionCallBack, pP, pieceType}) {
+  const [x, setX] = useState(defaultPosition.x);
+  const [y, setY] = useState(defaultPosition.y);
+  const [pieType, setPieceType] = useState(pieceType);
+  const [oldPie, setOldPiece] = useState("");
+  const startingPosition = { x: 23, y: 23 };
+  const boxSize = 70;
+  const bound = { left: 0, top: 0, right: 525, bottom: 525 };
+
   const handleStop = (event, dragElement) => {
-  let newX = Math.floor(dragElement.x /70) * boxSize + startingPosition.x 
-  let newY = Math.floor(dragElement.y /70) * boxSize + startingPosition.y 
-  let oldPoint = String.fromCharCode('a'.charCodeAt(0) + Math.floor(x /70)) + String(8 - Math.floor(y /70))
-  let newPoint = String.fromCharCode('a'.charCodeAt(0) + Math.floor(dragElement.x /70)) + String(8 - Math.floor(dragElement.y /70))
+    let newX = Math.floor(dragElement.x / 70) * boxSize + startingPosition.x;
+    let newY = Math.floor(dragElement.y / 70) * boxSize + startingPosition.y;
+    let oldPoint =
+      String.fromCharCode("a".charCodeAt(0) + Math.floor(x / 70)) +
+      String(8 - Math.floor(y / 70));
+    let newPoint =
+      String.fromCharCode("a".charCodeAt(0) + Math.floor(dragElement.x / 70)) +
+      String(8 - Math.floor(dragElement.y / 70));
 
-  if ( newPoint[1] === '8' && pieType === 'p' && color === 'white' || newPoint[1] === '1' && pieType === 'p' && color === 'black') {
-  if (chess.move({ from: oldPoint, to: newPoint, promotion:'q'}) !== null) {
-    setX(newX)
-    setY(newY)
-    setOldPiece(oldPoint)
-    callBack(color, newPoint)
-    chess.undo()
-    promotionCallBack(color, newPoint)
-  } else {
-    setX(x)
-    setY(y)
-    return
-  }
-} else {
-
-    if ( chess.move({ from: oldPoint, to: newPoint }) !== null) {
-      setX(newX)
-      setY(newY)
-      callBack(color, newPoint)
-    } else {
-      setX(x)
-      setY(y)
+    if (
+      (newPoint[1] === "8" && pieType === "p" && color === "white") ||
+      (newPoint[1] === "1" && pieType === "p" && color === "black")
+    ) {
+      if ( chess.move({ from: oldPoint, to: newPoint, promotion: "q" }) !== null ) {
+        setX(newX);
+        setY(newY);
+        setOldPiece(oldPoint);
+        callBack(color, newPoint);
+        chess.undo();
+        promotionCallBack(color, newPoint);
+      } 
+      else {
+        setX(x);
+        setY(y);
+        return;
+      }
+    } 
+    else {
+      if (chess.move({ from: oldPoint, to: newPoint }) !== null) {
+        setX(newX);
+        setY(newY);
+        callBack(color, newPoint);
+      } else {
+        setX(x);
+        setY(y);
+      }
     }
-  }
   };
 
-
   const handleStart = (event, dragElement) => {
-    document.getElementById(`drag-con-${i}-${color}`).style.zIndex = 1000
-  }
+    document.getElementById(`drag-con-${i}-${color}`).style.zIndex = 1000;
+  };
 
-
-  useEffect(()=>{
-    let oldPoint = String.fromCharCode('a'.charCodeAt(0) + Math.floor(x /70)) + String(8 - Math.floor(y /70))
-    if ( mP != null && color !== mP.color && oldPoint === mP.location) {
+  useEffect(() => {
+    let oldPoint = String.fromCharCode("a".charCodeAt(0) + Math.floor(x / 70)) + String(8 - Math.floor(y / 70));
+    if (mP != null && color !== mP.color && oldPoint === mP.location) {
       //hide piece
-      document.getElementById(`drag-con-${i}-${color}`).style.display = "none"
+      document.getElementById(`drag-con-${i}-${color}`).style.display = "none";
     }
-    if ( pP != null && color == pP.color && oldPoint == pP.location) {
+    if (pP != null && color == pP.color && oldPoint == pP.location) {
       //hide piece
-      chess.move({ from: oldPie, to: oldPoint, promotion:pP.type})
-      if ( color == 'white') document.getElementById(`piece-${i}-${color}`).src= whitePieces[pP.type]
-      else document.getElementById(`piece-${i}-${color}`).src= blackPieces[pP.type]
-      setPieceType(pP.type)
+      chess.move({ from: oldPie, to: oldPoint, promotion: pP.type });
+      if (color == "white")
+        document.getElementById(`piece-${i}-${color}`).src =
+          whitePieces[pP.type];
+      else
+        document.getElementById(`piece-${i}-${color}`).src = blackPieces[pP.type];
+      setPieceType(pP.type);
     }
-  }, )
-
+  });
 
   return (
     <div id={`drag-con-${i}-${color}`}>
-    <Draggable
-      defaultPosition={defaultPosition}
-      position={{x:x,y: y}}
-      bounds={bound}
-      scale={1}
-      allowAnyClick={true}
-      id="draggable"
-      onStop={handleStop} 
-      onStart={handleStart} 
-    >
-      <img src={source} alt="coffee" className="piece" id={`piece-${i}-${color}`}/>
-    </Draggable>
+      <Draggable
+        defaultPosition={defaultPosition}
+        position={{ x: x, y: y }}
+        bounds={bound}
+        scale={1}
+        allowAnyClick={true}
+        id="draggable"
+        onStop={handleStop}
+        onStart={handleStart}
+      >
+        <img
+          src={source}
+          alt="coffee"
+          className="piece"
+          id={`piece-${i}-${color}`}
+        />
+      </Draggable>
     </div>
   );
 }
